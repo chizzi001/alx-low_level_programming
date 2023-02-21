1ad84614e022bb18a4a7197b778e470cefc1d68f
chizzi001 <chidiobinna0001@gmail.com>
2023-02-21 00:15:23 -0800

    absolute values of integers

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _abs - compute absolute value of integer
+ *
+ * @n: an integer
+ *
+ * Return: absolute value of @n
+ */
+int _abs(int n)
+{
+	return (abs(n));
+}
